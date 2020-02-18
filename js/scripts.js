acec54353db3cda441ec5fa2c7512c1ac9553fe5
19293 <chkouassi@gmail.com>
2020-02-18 10:08:42 -0800

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*set the carousel image cycle interval to 2000 ms, and implement pause*/
+
+    $(function() {
+        $(".carousel").carousel( { interval: 2000 } );
+        $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause"); 
+            }
+        });
+    });
